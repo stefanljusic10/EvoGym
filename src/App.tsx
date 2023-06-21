7427cd7533d6b423bb2139bce8c53e4cf2aa1ea6
unknown <stefanljusic10@gmail.com>
2023-06-21 23:20:07 +0200

    nav

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import Navbar from '@/components/navbar'
+import { SelectedPage } from '@/shared/types'
+
+const App = () => {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  )
+}
+
+export default App
