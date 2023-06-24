aa46f739103dae2c904225495fb0e98a3b9c71e7
unknown <stefanljusic10@gmail.com>
2023-06-24 22:03:47 +0200

    our classes

---
+++ src/components/ourClasses/index.tsx
@@ -0,0 +1,25 @@
+import { SelectedPage } from '@/shared/types'
+import React from 'react'
+import image1 from '@/assets/image1.png'
+import image2 from '@/assets/image2.png'
+import image3 from '@/assets/image3.png'
+import image4 from '@/assets/image4.png'
+import image5 from '@/assets/image5.png'
+import image6 from '@/assets/image6.png'
+import { motion } from 'framer-motion'
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+}
+
+const index = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="ourclasses" className='w-full bg-primary-100 py-40'>
+        <motion.div>
+            
+        </motion.div>
+    </section>
+  )
+}
+
+export default index
