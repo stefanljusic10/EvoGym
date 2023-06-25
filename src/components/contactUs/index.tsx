70d7c0e7ccbe2cbc2d9565e6bf8b798bc3e7f432
unknown <stefanljusic10@gmail.com>
2023-06-26 00:44:33 +0200

    contact us form

--- src/components/contactUs/index.tsx
+++ src/components/contactUs/index.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const index = ({ setSelectedPage }: Props) => {
   const inputStyles =
-    "w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white";
+    "mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white";
 
   const {
     register,
@@ -30,6 +30,7 @@ const index = ({ setSelectedPage }: Props) => {
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
       >
+        {/* HEADER */}
         <motion.div
           className="md:w-3/5"
           initial="hidden"
@@ -39,16 +40,16 @@ const index = ({ setSelectedPage }: Props) => {
           variants={headlineVariant}
         >
           <HeaderText>
-            <span className="text-primary-500">JOIN NOW </span>
-            TO GET IN SHAPE
+            <span className="text-primary-500">JOIN NOW</span> TO GET IN SHAPE
           </HeaderText>
           <p className="my-5">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
-            mollitia nulla, fugit obcaecati beatae quibusdam quos explicabo
-            cumque vitae ullam neque quas unde culpa maxime non? Distinctio
-            vitae et, soluta accusamus blanditiis neque consequuntur beatae!
+            Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+            sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+            adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
           </p>
         </motion.div>
+
+        {/* FORM AND IMAGE */}
         <div className="mt-10 justify-between gap-8 md:flex">
           <motion.div
             className="mt-10 basis-3/5 md:mt-0"
@@ -61,7 +62,7 @@ const index = ({ setSelectedPage }: Props) => {
             <form
               target="_blank"
               onSubmit={(e) => submitForm(e)}
-              action="https://formsubmit.co/stefan.e4e5@email.com"
+              action="https://formsubmit.co/b5d3940226a4e1779406608be01db181"
               method="POST"
             >
               <input
@@ -72,53 +73,75 @@ const index = ({ setSelectedPage }: Props) => {
                   required: true,
                   maxLength: 100,
                 })}
-              >
-                {errors.name && (
-                  <p className="mt-1 text-primary-500">
-                    {errors.name.type === "required" &&
-                      "This field is required."}
-                    {errors.name.type === "maxLength" &&
-                      "Max length is 100 characters."}
-                  </p>
-                )}
-              </input>
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 char."}
+                </p>
+              )}
+
               <input
                 className={inputStyles}
                 type="text"
                 placeholder="EMAIL"
                 {...register("email", {
                   required: true,
-                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                 })}
-              >
-                {errors.email && (
-                  <p className="mt-1 text-primary-500">
-                    {errors.email.type === "required" &&
-                      "This field is required."}
-                    {errors.email.type === "pattern" &&
-                      "Invalid email address."}
-                  </p>
-                )}
-              </input>
-              <input
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              <textarea
                 className={inputStyles}
-                type="text"
                 placeholder="MESSAGE"
+                rows={4}
+                cols={50}
                 {...register("message", {
                   required: true,
                   maxLength: 2000,
                 })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 char."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
               >
-                {errors.message && (
-                  <p className="mt-1 text-primary-500">
-                    {errors.message.type === "required" &&
-                      "This field is required."}
-                    {errors.message.type === "maxLength" &&
-                      "Max length is 2000 characters."}
-                  </p>
-                )}
-              </input>
+                SUBMIT
+              </button>
             </form>
+          </motion.div>
+
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={headlineInverseVariant}
+          >
+            <div className="w-full before:absolute before:-bottom-20 before:-right-10 before:z-[-1] md:before:content-evolvetext">
+              <img
+                className="w-full"
+                alt="contact-us-page-graphic"
+                src={ContactUsPageGraphic}
+              />
+            </div>
           </motion.div>
         </div>
       </motion.div>
--- src/components/ourClasses/index.tsx
+++ src/components/ourClasses/index.tsx
@@ -1,5 +1,4 @@
 import { SelectedPage } from "@/shared/types";
-import React from "react";
 import image1 from "@/assets/image1.png";
 import image2 from "@/assets/image2.png";
 import image3 from "@/assets/image3.png";
