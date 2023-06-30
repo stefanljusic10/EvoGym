32b2670170e0bc03e16f850a671fac820a38e0ec
unknown <stefanljusic10@gmail.com>
2023-06-30 16:51:54 +0200

    footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,9 +1,33 @@
 import Logo from "@/assets/Logo.png";
 
-type Props = {};
-
-const index = (props: Props) => {
-  return <footer className="bg-primary-100 py-16">index</footer>;
+const index = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} />
+          <p className="my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa iste
+            tempore debitis corrupti molestias inventore voluptates quis atque
+            distinctio perferendis?
+          </p>
+          <p>&copy; Evogym All Rights Reserved.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+            <h4 className="font-bold">Links</h4>
+            <p className="my-5">Lorem, ipsum dolor.</p>
+            <p className="my-5">Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit.</p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+            <h4 className="font-bold">Contact us</h4>
+            <p className="my-5">Lorem, ipsum dolor.</p>
+            <p>+(111)343-4343</p>
+        </div>
+        <div></div>
+      </div>
+    </footer>
+  );
 };
 
 export default index;
